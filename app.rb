3cf6c153437bed97f40d9b5fc5e74d5127e14892
Valdemar <gravemaggot1@gmail.com>
2020-05-26 21:25:15 +0300

    end of 29-th task

--- app.rb
+++ app.rb
@@ -12,7 +12,30 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do
 	@barbers = Barber.all
+end
+
+get '/' do
 	erb :index			
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+get '/visit' do
+    erb :visit
+end
+
+post '/visit' do
+    @username  = params[:username]; 
+    @phone     = params[:phone];
+	@datetime  = params[:datetime];
+    @master    = params[:master];
+    @color     = params[:color];
+
+    @title     = 'Thanks you!';
+    @message   = "Dear #{@user_name}, we'll be waiting for you at #{@datetime}. Master: #{@master}. Color: #{@color}";
+
 end
