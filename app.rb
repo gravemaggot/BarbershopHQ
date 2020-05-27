a9a5cc9f3e072ba5f766c42e8beea780674523d4
Valdemar <gravemaggot1@gmail.com>
2020-05-27 21:19:53 +0300

    Homework done!

--- app.rb
+++ app.rb
@@ -12,6 +12,9 @@ end
 class Barber < ActiveRecord::Base
 end
 
+class Feedback < ActiveRecord::Base
+end
+
 before do
 	@barbers = Barber.all
 end
@@ -36,6 +39,58 @@ post '/visit' do
     @color     = params[:color];
 
     @title     = 'Thanks you!';
-    @message   = "Dear #{@user_name}, we'll be waiting for you at #{@datetime}. Master: #{@master}. Color: #{@color}";
+    @message   = "Dear #{@username}, we'll be waiting for you at #{@datetime}. Master: #{@master}. Color: #{@color}";
 
+   # Проверка заполнения
+   errPattern = {
+        :username   => 'Введите имя',
+        :phone      => 'Укажите телефон',
+        :datetime   => 'Не правильная дата визита'
+    };
+
+    @error = errPattern.select {|key,_| params[key] == ''}.values.join(", "); 
+
+    if @error != '' 
+        return erb :visit 
+    end;
+
+    # Запись в файл
+    if write_client(@username,@phone,@datetime,@master,@color) 
+        erb :message 
+    else
+        @error = 'Ошибка записи. Попробуйте еще раз.';
+        erb :visit 
+    end
+
+end
+
+post '/contacts' do
+  @content = params[:content]
+
+  if @content == ''
+    @error = 'Не введен текст сообщения!'
+    return erb :contacts
+  end
+
+  if write_feedback(@content)
+    @message = 'Спасибо. Ваш отзыв очень важен для нас'
+    erb :message
+  else
+    @error = 'Ошибка записи. Попробуйте еще раз.'
+    erb :contacts
+  end
+end
+
+def write_client(username,phone,datestamp,barber,color)
+  Client.create  :name      => username, 
+                 :phone     => phone,
+                 :datestamp => datestamp,
+                 :barber    => barber,
+                 :color     => color;
+    true
+end
+
+def write_feedback(content)
+  Feedback.create :content => content
+  true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_173638) do
+ActiveRecord::Schema.define(version: 2020_05_27_175546) do
 
   create_table "barbers", force: :cascade do |t|
     t.text "name"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2020_05_26_173638) do
     t.text "datestamp"
     t.text "barber"
     t.text "color"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
